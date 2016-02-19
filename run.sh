285ffb052fbf163d877af17d93a49a447f3887f3
Aarti <aartibagul95@gmail.com>
2016-02-19 17:20:26 -0500

    Added bash script to either tag or search

---
+++ run.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+show_help() { 
+        echo -e "\nUsage:\n$0 -c {tag|search} -d {/path/to/directory}  \n" 
+}
+ 
+
+if [ $# -lt 1 ]; then
+   show_help
+   exit 1
+fi
+
+#name=1
+
+
+#echo $name
+
+OPTIND=1         # Reset in case getopts has been used previously in the shell.
+
+# Initialize our own variables:
+directory_path=""
+
+while getopts "h?:c:d:" opt; do
+    case "$opt" in
+    h|\?)
+        show_help
+        exit 0
+        ;;
+    c)  command_name=$OPTARG
+        if [ "$OPTARG" == "tag" ] 
+        then
+            tag= 1
+
+        elif [ "$OPTARG" == "search" ]
+        then
+            tag=0
+        else
+            echo "Invalid command"
+            show_help
+            exit 1
+        fi
+        ;;
+    d)  directory_path=$OPTARG
+        ;;
+    esac
+done
+
+shift $((OPTIND-1))
+
+[ "$1" = "--" ] && shift
+
+if [ $tag -eq 1 ]
+then
+    python tag.py $directory_path
+else
+    python search.py $directory_path
+
+echo "output_file='$directory_path', command = '$command_name', Leftovers: $@"
+
